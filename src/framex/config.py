5f21ef5a029f242eeb40dace09c126f77d3dcdae
touale <136764239@qq.com>
2025-07-14 12:17:05 +0800

    feat: Add multi TOML and YAML support for plugin configs

--- src/framex/config.py
+++ src/framex/config.py
@@ -1,13 +1,53 @@
+from pathlib import Path
 from typing import Any
 
+import tomli
+import yaml
 from pydantic import BaseModel
+from pydantic.fields import FieldInfo
 from pydantic_settings import (
     BaseSettings,
     PydanticBaseSettingsSource,
     PyprojectTomlConfigSettingsSource,
     SettingsConfigDict,
     TomlConfigSettingsSource,
+    YamlConfigSettingsSource,
 )
+
+
+class PluginConfigSource(PydanticBaseSettingsSource):
+    def __init__(self, settings_cls: type[BaseSettings], data_dir: Path = Path("data")):
+        super().__init__(settings_cls)
+        self.data_dir = data_dir
+        self._data: dict[str, Any] = self._load_plugins()
+
+    def _load_plugins(self) -> dict[str, Any]:
+        plugin_configs: dict[str, dict] = {}
+
+        if not self.data_dir.is_dir():
+            return {}
+
+        for plugin_dir in self.data_dir.iterdir():
+            if not plugin_dir.is_dir():
+                continue
+
+            name = plugin_dir.name.split("@")[0]
+            for file in plugin_dir.iterdir():
+                if file.suffix in (".yaml", ".yml"):
+                    with file.open("r", encoding="utf-8") as f:
+                        plugin_configs[name] = yaml.safe_load(f)
+                    break
+                if file.suffix == ".toml":
+                    with file.open("rb") as f:
+                        plugin_configs[name] = tomli.load(f)
+                    break
+        return {"plugins": plugin_configs}
+
+    def __call__(self) -> dict[str, Any]:
+        return self._data
+
+    def get_field_value(self, field: FieldInfo, field_name: str) -> tuple[Any, str, bool]:
+        raise NotImplementedError
 
 
 class LogConfig(BaseModel):
@@ -36,7 +76,6 @@ class Settings(BaseSettings):
     enable_proxy: bool = False
     plugins: dict[str, Any] = {}
     log: LogConfig = LogConfig()
-
     load_plugins: list[str] = []
     load_builtin_plugins: list[str] = []
 
@@ -61,7 +100,9 @@ class Settings(BaseSettings):
     ) -> tuple[PydanticBaseSettingsSource, ...]:
         return (
             env_settings,
+            PluginConfigSource(settings_cls),
             TomlConfigSettingsSource(settings_cls),
+            YamlConfigSettingsSource(settings_cls),
             PyprojectTomlConfigSettingsSource(settings_cls),
             dotenv_settings,
             init_settings,
